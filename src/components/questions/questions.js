9331d282d27240de1ca34f02bcf3d108a25eefde
Marko Cekaj <marko.cekaj@amplitudo.me>
2021-05-04 09:38:16 +0200

    list of questions

---
+++ src/components/questions/questions.js
@@ -0,0 +1,202 @@
+export const questions = [
+  {
+    id: 1,
+    question: "Capital of Nigeria",
+    answers: [
+      { answer: "Abeokuta", correct: false },
+      { answer: "Lagos", correct: false },
+      { answer: "Abuja", correct: true },
+      { answer: "Calabar", correct: false },
+    ],
+  },
+  {
+    id: 2,
+    question: "Capital of Algeria",
+    answers: [
+      { answer: "Oran", correct: false },
+      { answer: "Constantine", correct: false },
+      { answer: "Algiers", correct: true },
+      { answer: "Chlef", correct: false },
+    ],
+  },
+  {
+    id: 3,
+    question: "Capital of Benin",
+    answers: [
+      { answer: "Ganvie", correct: false },
+      { answer: "Porto-Novo", correct: true },
+      { answer: "Parakou", correct: false },
+      { answer: "Abomey", correct: false },
+    ],
+  },
+  {
+    id: 4,
+    question: "Capital of Botswana",
+    answers: [
+      { answer: "Gaborne", correct: true },
+      { answer: "Molepolole", correct: false },
+      { answer: "Serowe", correct: false },
+      { answer: "Maun", correct: false },
+    ],
+  },
+  {
+    id: 5,
+    question: "Capital of Burundi",
+    answers: [
+      { answer: "Cibitoke", correct: false },
+      { answer: "Kisozi", correct: false },
+      { answer: "Muramvya", correct: false },
+      { answer: "Gitega", correct: true },
+    ],
+  },
+  {
+    id: 6,
+    question: "Capital of Cape Verde",
+    answers: [
+      { answer: "Mindelo", correct: false },
+      { answer: "Espargos", correct: false },
+      { answer: "Assomada", correct: false },
+      { answer: "Praia", correct: true },
+    ],
+  },
+  {
+    id: 7,
+    question: "Capital of Central African Republic",
+    answers: [
+      { answer: "Baoro", correct: false },
+      { answer: "Bangui", correct: true },
+      { answer: "Bouar", correct: false },
+      { answer: "Gambo", correct: false },
+    ],
+  },
+  {
+    id: 8,
+    question: "Capital of Chad",
+    answers: [
+      { answer: "N'Djamena", correct: true },
+      { answer: "Abou-Deia", correct: false },
+      { answer: "Beinamar", correct: false },
+      { answer: "Bokoro", correct: false },
+    ],
+  },
+  {
+    id: 9,
+    question: "Capital of DRC",
+    answers: [
+      { answer: "Bumba", correct: false },
+      { answer: "Kinshasa", correct: true },
+      { answer: "Isiro", correct: false },
+      { answer: "Kindu", correct: false },
+    ],
+  },
+  {
+    id: 10,
+    question: "Capital of Congo",
+    answers: [
+      { answer: "Louborno", correct: false },
+      { answer: "Oyo", correct: false },
+      { answer: "Owando", correct: false },
+      { answer: "Brazzaville", correct: true },
+    ],
+  },
+  {
+    id: 11,
+    question: "Capital of Ivory Coast",
+    answers: [
+      { answer: "sinfra", correct: false },
+      { answer: "San-Pedro", correct: false },
+      { answer: "Yamoussoukro", correct: true },
+      { answer: "Touba", correct: false },
+    ],
+  },
+  {
+    id: 12,
+    question: "Capital of Egypt",
+    answers: [
+      { answer: "Alexandria", correct: false },
+      { answer: "Cairo", correct: true },
+      { answer: "Giza", correct: false },
+      { answer: "Port Said", correct: false },
+    ],
+  },
+  {
+    id: 13,
+    question: "Capital of Gabon",
+    answers: [
+      { answer: "Mouila", correct: false },
+      { answer: "Libreville", correct: true },
+      { answer: "Bitam", correct: false },
+      { answer: "Oyern", correct: false },
+    ],
+  },
+  {
+    id: 14,
+    question: "Capital of Gambia",
+    answers: [
+      { answer: "Bakau", correct: false },
+      { answer: "Banjul", correct: true },
+      { answer: "Brikama", correct: false },
+      { answer: "Gunjur", correct: false },
+    ],
+  },
+  {
+    id: 15,
+    question: "Capital of Liberia",
+    answers: [
+      { answer: "Kakata", correct: false },
+      { answer: "Harbel", correct: false },
+      { answer: "Monrovia", correct: true },
+      { answer: "River Cess", correct: false },
+    ],
+  },
+  {
+    id: 16,
+    question: "Capital of Libya",
+    answers: [
+      { answer: "Benghazi", correct: false },
+      { answer: "Tripolis", correct: true },
+      { answer: "Tobruk", correct: false },
+      { answer: "Zuwara", correct: false },
+    ],
+  },
+  {
+    id: 17,
+    question: "Capital of Rwanda",
+    answers: [
+      { answer: "Butare", correct: false },
+      { answer: "Kibungo", correct: false },
+      { answer: "Byumba", correct: false },
+      { answer: "Kigali", correct: true },
+    ],
+  },
+  {
+    id: 18,
+    question: "Capital of Senegal",
+    answers: [
+      { answer: "Kolda", correct: false },
+      { answer: "Dakar", correct: true },
+      { answer: "Bignona", correct: false },
+      { answer: "Thies", correct: false },
+    ],
+  },
+  {
+    id: 19,
+    question: "Capital of Sierra Leone",
+    answers: [
+      { answer: "Kenama", correct: false },
+      { answer: "Bo", correct: false },
+      { answer: "Freetown", correct: true },
+      { answer: "Koidu", correct: false },
+    ],
+  },
+  {
+    id: 20,
+    question: "Capital of Somalia",
+    answers: [
+      { answer: "Baidoa", correct: false },
+      { answer: "Mogadishu", correct: true },
+      { answer: "Galcaio", correct: false },
+      { answer: "Boosaaso", correct: false },
+    ],
+  },
+];
